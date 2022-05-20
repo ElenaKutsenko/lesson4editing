fe9bd46c052027859d24e15ed5e159b457506db0
Elena Kutsenko <albion.k@mail.ru>
2022-05-20 20:55:15 +0300

    Новые правки

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int i = 1;
         while (i <= 10) {
-            System.out.print(i);
+            System.out.print(i+" ");
             i = i + 1;
         }
         {System.out.println ("\n");
         }
 
         for (int b =10;b>=1; b =b-1){
-            System.out.print(b);
+            System.out.print(b+" ");
 
         }
         {System.out.println ("\n");
@@ -17,13 +17,15 @@ public class Main {
 
         int friday = 6;
         while (friday <= 31) {
-            System.out.println("Ñåãîäíÿ ïÿòíèöà," + friday+ "-å ÷èñëî. Íåîáõîäèìî ïîäãîòîâèòü îò÷åò.");
+            System.out.println("Ð¡ÐµÐ³Ð¾Ð´Ð½Ñ Ð¿ÑÑ‚Ð½Ð¸Ñ†Ð°," + friday+ "-Ðµ Ñ‡Ð¸ÑÐ»Ð¾. ÐÐµÐ¾Ð±Ñ…Ð¾Ð´Ð¸Ð¼Ð¾ Ð¿Ð¾Ð´Ð³Ð¾Ñ‚Ð¾Ð²Ð¸Ñ‚ÑŒ Ð¾Ñ‚Ñ‡ÐµÑ‚.");
             friday = friday + 7;
 
         }
         int now = 2021;
         int future = now+100;
-        for (int year =1896;year<=future; year= year+79) {
+        int before = now-200;
+        for (int year =0;year<=2121; year= year+79)
+        if (year>=before&& year<=future) {
             System.out.println(year);
 
 
